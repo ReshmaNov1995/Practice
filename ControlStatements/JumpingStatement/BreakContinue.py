38ee27f30bd12e4ace623265067b237c7c9d3ebc
ATE188 <reshma.s@agaramtech.com>
2022-07-13 19:04:01 +0530

    commit

--- ControlStatements/JumpingStatement/BreakContinue.py
+++ ControlStatements/JumpingStatement/BreakContinue.py
@@ -8,7 +8,7 @@ print("Exit Program")
 
 # Continue
 for i in range(1,10):
-    if i == 5:
-        continue # skip the below part and continue with the next iteration
+    if i == 5 or i == 3 or i == 7: # skip the below part and continue with the next iteration. Multiple conditions can set.
+        continue
     print(i)
 print("Exit Program")
