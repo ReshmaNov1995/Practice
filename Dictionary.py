4b10659f2ce40a65db46f1a87527b0b90d2afd80
ATE188 <reshma.s@agaramtech.com>
2022-07-22 18:43:33 +0530

    commit

---
+++ Dictionary.py
@@ -0,0 +1,69 @@
+# Example 1: Creating dictionary
+
+dict1 = {1:"Reshma", 2:"Roja", 3:"Rekha", 4:"Ranjith"}
+print(dict1)
+
+# Example 2: Accessing Item from dictionary
+
+# Method - 1:
+dict2 = {"Name": "Roja", "Year": 1995, "Month": "September", "Passing": 2017}
+print(dict2["Month"])
+
+# Method - 2: using get()
+print(dict1.get(2))
+
+# Example 3: Change values in dictionary
+
+dict1[3] = "Reeta"
+print(dict1)
+
+# Example - 4: Reading items from dictionary using loop
+
+# Printing keys
+for i in dict1:
+    print(i)
+
+# Printing values
+for i in dict1:
+    print(dict1[i])
+
+# Printing values using values()
+for i in dict1.values():
+    print(i)
+
+# To Print keys along with Values - items()
+for x,y in dict1.items():
+    print(x,y)
+
+# Example 5: Check key exist in dictionary
+
+if 2 in dict1:
+    print("Exist")
+else:
+    print("Not Exist")
+
+# OR
+
+print(2 in dict1)
+
+# Example 6: Find number of items in dictionary
+
+print(len(dict2))
+
+# Example 7: Adding Items in dictionary
+
+dict1[5] = "Rakul"
+print(dict1)
+
+# Example 8: Remove Item from dictionary
+
+# Method - 1: using pop()
+dict1.pop(3)
+print(dict1)
+
+# Method - 2: del keyword
+del dict1[4]
+print(dict1)
+
+del dict1
+# print(dict1)
--- Set.py
+++ Set.py
@@ -16,3 +16,47 @@ print("Reshma" in set2)
 
 # Example - 4: Add items to Set
 
+# add() - Add singe item
+set2.add("rishen")
+print(set2)
+
+# update() - Add Multiple item
+set2.update(["Reshma", "Roja", "Rishaba"])
+print(set2)
+
+# Example - 5: Set length
+
+print(len(set2))
+
+# Example - 6: Remove item from set
+# Both remove() and discard() ll perform same.
+# Only difference is, discard the non-existing value in the set won't throw error.
+
+# Method - 1: remove()
+set2.remove("Roja")
+print(set2)
+
+# Method - 2: discard()
+set2.discard("Reshma")
+print(set2)
+
+# Example - 7: Clear the items in set
+
+set3 = {"fear", "afraid", "unease"}
+# set3.clear()
+print(set3)
+
+del set3
+# print(set3)
+
+# Example - 8: Join Set
+
+# Method - 1: union()
+set4 = {12, 98, "icecream", 652, "chocolate"}
+set5 = {"temptation", "FerreroRocher", "KinderJoy"}
+set6 = set4.union(set5)
+print(set6)
+
+# Method - 2: update()
+set4.update(set5)
+print(set4)
