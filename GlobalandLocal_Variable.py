c7b7612c1eb1a58edcf4ab515c54806fd18a4ee0
ATE188 <reshma.s@agaramtech.com>
2022-08-22 19:18:22 +0530

    Update GlobalandLocal_Variable.py

---
+++ GlobalandLocal_Variable.py
@@ -0,0 +1,37 @@
+# # Example - 1:
+#
+# a = 20
+#
+# def fun():
+#     print(a)
+#
+#     b = 30
+#     print(b)
+#
+# fun()
+#
+# # print(b) # It throws error bcoz it's a local variable.
+
+# # Example - 2:
+#
+# # Both Global and Local variable has same name. Inside the function Local variable 'll be considered.
+#
+# ab = 50
+#
+# def fun1():
+#     ab = 100
+#     print(ab)
+#
+# fun1()
+
+# Example - 3:
+
+ab = 50
+
+def fun1():
+    global ab # Local variable is mentioned as a global variable
+    ab = 100
+    print(ab) # 100
+
+fun1()
+print(ab) # 100
