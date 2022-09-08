cbd305550ac9b7fd5682fb7d6fb8e82c563f328d
ATE188 <reshma.s@agaramtech.com>
2022-09-08 18:25:44 +0530

    Update Class.py

--- OOPS/Class.py
+++ OOPS/Class.py
@@ -1,4 +1,6 @@
 # creating a class
+# Example 1:
+
 class MyClass:
     def methodcase(self): # Instance Method - self is a default argument taken by method which represents the class name. Static Method will consider as a argument alone.
         pass
@@ -28,21 +30,58 @@ mc2.methodcase1()
 mc2.methodcase2(25)
 
 # Static Method & Non-Static(Instance) Method
+# Example 2:
 
-class static:
+class Static:
     def imethod(self):
         print("Am Instance Method")
     @staticmethod
     def smethod(self):
-        print(self)
+        print()
 
 
-static.smethod(20)
+Static.smethod(1)
 
-s = static()
+s = Static()
 s.smethod(30)
 
+# Example 3:
+# Class Variable
 
+class Class:
+    a, b = 10, 20
+    def add(self):
+        print(self.a + self.b) # class variable must accessed inside a method by using self keyword
 
+c = Class()
+c.add()
 
+# Example 4:
+# Diff b/w Global, Class & Local Variable
 
+i, j = 20, 30 #Global Variable
+class VariableClass:
+    a, b = 10, 20 #Class Variable
+    def method(self):
+        x, y = 50, 100 #Local Variable
+        print(x + y)
+        print(self.a + self.b)
+        print(i + j)
+
+vc = VariableClass()
+vc.method()
+
+# Example 5:
+# Global, Class & Local Variable with same name
+
+m, n = 20, 30 #Global Variable
+class VariableClass1:
+    m, n = 10, 20 #Class Variable
+    def method(self):
+        m, n = 50, 100 #Local Variable
+        print(m + n) #local
+        print(self.m + self.n) #class
+        print(globals()['m'] + globals()['n']) #global
+
+vc = VariableClass1()
+vc.method()
