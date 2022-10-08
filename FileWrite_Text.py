58c063a065cb404c13c7f2f837fb0439295102b5
ATE188 <reshma.s@agaramtech.com>
2022-10-08 17:28:49 +0530

    Create FileWrite_Text.py

---
+++ FileWrite_Text.py
@@ -0,0 +1,26 @@
+# File Write
+
+textfile = open("D:\sample\TextFile.txt", "w")
+
+line1 = "Hi Reshu how are you\n"
+
+line2 = "How is your Automation scripts Practice"
+
+textfile.write("Hey!!!\n")
+textfile.writelines((line1, line2))
+
+textfile.flush()
+#
+# # File Read
+
+# textfile = open("D:\sample\TextFile.txt", "r")
+#
+# # print(textfile.read()) # It 'll read the entire file
+# print(textfile.readline()) # It 'll read only line of the file
+
+# File Append ---> It is used to write at the End of the File.
+textfile = open("D:\sample\TextFile.txt", "a")
+
+textfile.write("\nWhat's Up")
+textfile = open("D:\sample\TextFile.txt", "r")
+print(textfile.read())
